03188af5cd6385c6df418c4b531cbe684d1b96fd
Mukramin <sardermukramen@gmail.com>
2024-09-27 23:34:48 +0600

    initial commit

---
+++ test/bin/test.dart
@@ -0,0 +1,18 @@
+class Car{
+  String Brand;
+  String Model;
+  int Year;
+  Car(this.Brand,this.Model,this.Year){
+  }
+  int Car_age(){
+    return (DateTime.now().year-Year);
+  }
+}
+main(){
+  Car car1=Car("Toyota","20zx6127",2000);
+  print("Brand:${car1.Brand}");
+  print("Model:${car1.Model}");
+  print("Year:${car1.Year}");
+  print("Car Age:${car1.Car_age()}");
+
+}
